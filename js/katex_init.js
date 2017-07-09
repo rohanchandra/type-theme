cbff4168c030b1d424ff91e92b96b33eb0bff3aa
Rohan Chandra <rohanchandragithub@gmail.com>
2017-07-09 12:22:34 +1000

    Fixes missing semicolon
    Link added to GitHub issue

--- js/katex_init.js
+++ js/katex_init.js
@@ -4,8 +4,11 @@ Array.prototype.forEach.call(elements, function(element) {
   if (element.type.indexOf('math/tex') != -1) {
      // Extract math markdown
      var textToRender = element.innerText || element.textContent;
-     textToRender = textToRender.replace(/%.*/g, '')
-    
+
+     // Kramdown and KaTeX workaround
+     // https://github.com/rohanchandra/type-theme/issues/47
+     textToRender = textToRender.replace(/%.*/g, '');
+
      // Create span for KaTeX
      var katexElement = document.createElement('span');
 
